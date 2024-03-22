255ea6950a192b9bf8c2d6730dc6aa38f861ddf3
Foizo <boneofsin@gmai.com>
2024-03-22 08:44:55 +0100

    udpates

--- src/Doctrine/Repository/ShortcutsUrlsRepository.php
+++ src/Doctrine/Repository/ShortcutsUrlsRepository.php
@@ -12,7 +12,7 @@ class ShortcutsUrlsRepository extends DefaultRepository
     const CACHE_TTL = 3600;
     const CACHE_KEY_PREFIX = 'shortcut-';
 
-    function findOneByGeneratedOrCustomerShortcut(AppDomain $appDomain, string $shortcut, bool $allow_cache = true): ?ShortcutUrl
+    function findOneByGeneratedOrCustomerShortcut(AppDomain $appDomain, string $shortcut): ?ShortcutUrl
     {
         $qb = $this->createQueryBuilder('short')
             ->where('short.app_domain = :app_domain')
@@ -24,10 +24,6 @@ class ShortcutsUrlsRepository extends DefaultRepository
                 'shortcut' => $shortcut
             ])
             ->getQuery();
-
-        if ($allow_cache) {
-            $qb->enableResultCache(self::CACHE_TTL, $appDomain->identifier . '-' . self::CACHE_KEY_PREFIX . $shortcut);
-        }
 
         return $qb->getOneOrNullResult();
     }
